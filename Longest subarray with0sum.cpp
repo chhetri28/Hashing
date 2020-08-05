e844e07b1e75bbe4226d279faea82fdcdf5ef3ad
chhetri28 <chettrisourav28@gmail.com>
2020-08-05 19:06:09 +0530

    Longest subarray with sum 0

--- Longest subarray with0sum.cpp
+++ Longest subarray with0sum.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int longestsub(int *a,int n){
     unordered_map<int,int>m;
-    int pre=0;
+    int pre=0;// for prefix sum
     int len=0;
     for(int i=0;i<n;i++){
-        pre+=a[i];
-        if(pre==0){
-            len=max(len,i+1);
+        pre+=a[i];// add 1 by 1 all the element
+        if(pre==0){// check if any element is 0
+            len=max(len,i+1);// update the index
         }
-        if(m.find(pre)!=m.end()){
+        if(m.find(pre)!=m.end()){//if any element is already present then use older value
             len=max(len,i-m[pre]);
         }
         else{
-            m[pre]=i;
+            m[pre]=i;// update the index
         }
     }
     return len;
